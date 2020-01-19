ca30da3a60ce27816744c03db860eb291af41dc0
Orenef11 <Orenef11@gmail.com>
2020-01-20 00:54:43 +0200

    Update args_parser.py

--- tools/args_parser.py
+++ tools/args_parser.py
@@ -3,7 +3,7 @@ import argparse
 
 def command_line_arguments():
     parser = argparse.ArgumentParser(description="Welcome to Oren's simulate stress on the ScyllaDB database")
-    parser.add_argument('--stress_duration', help='How long the stress will be applied (In seconds)', type=int,
-                        required=False)
+    parser.add_argument('--stress_duration', type=int, help='How long the stress will be applied (In seconds)',
+                        required=True)
 
     return parser.parse_args()
